c02cfb3457c13f02998f46a6e2666bc1ae9b3281
vivekkm1989 <52948784+vivekkm1989@users.noreply.github.com>
2019-07-23 14:46:37 +0530

    Update webcomponent_Plain.js

--- webcomponent_Plain.js
+++ webcomponent_Plain.js
@@ -129,7 +129,7 @@
                                     }
                                 }                              
                             }  
-                          
+                          document.getElementById("Las Vegas").fillStyle="red";
                         });
 
 
