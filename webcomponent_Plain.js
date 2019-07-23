13c56d74e674263475387b57d585036595c2731b
vivekkm1989 <52948784+vivekkm1989@users.noreply.github.com>
2019-07-23 15:09:41 +0530

    Update webcomponent_Plain.js

--- webcomponent_Plain.js
+++ webcomponent_Plain.js
@@ -129,7 +129,7 @@
                                     }
                                 }                              
                             }  
-                         console.log(document.getElementById('rect').getSVGDocument());
+                         console.log(document.getElementById("rect"));
                         });
 
 
