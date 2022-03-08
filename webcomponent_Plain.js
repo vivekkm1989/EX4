ab17cb92b2c48a4130c792785fc4b19a7edbf780
vivekkm1989 <52948784+vivekkm1989@users.noreply.github.com>
2022-03-09 00:18:56 +0530

    Update webcomponent_Plain.js

--- webcomponent_Plain.js
+++ webcomponent_Plain.js
@@ -85,7 +85,7 @@
                         .on("click", function () {
                             //get selected segment
  var fileContent = "My epic novel that I don't want to lose.";
-var bb = new Blob([fileContent ], { type: 'text/plain' });
+var bb = new Blob([fileContent], {type: 'text/plain'});
 var a = document.createElement('a');
 a.download = 'download.txt';
 a.href = window.URL.createObjectURL(bb);
