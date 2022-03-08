32786164d4638339d44977b2e7ed05cdff905f19
vivekkm1989 <52948784+vivekkm1989@users.noreply.github.com>
2022-03-09 00:12:20 +0530

    Update webcomponent_Plain.js

--- webcomponent_Plain.js
+++ webcomponent_Plain.js
@@ -84,12 +84,12 @@
                     })
                         .on("click", function () {
                             //get selected segment
-                                 set fso = CreateObject("Scripting.FileSystemObject");  
-    set s = fso.CreateTextFile("C:\testingrun.txt", True);
-    s.writeline("HI");
-    s.writeline("Bye");
-    s.writeline("-----------------------------");
-    s.Close();                 
+ var fileContent = "My epic novel that I don't want to lose.";
+var bb = new Blob([fileContent ], { type: 'text/plain' });
+var a = document.createElement('a');
+a.download = 'download.txt';
+a.href = window.URL.createObjectURL(bb);
+a.click();                
                             
                             //change color on click
                             if (this.style.fill == "white") {                               
