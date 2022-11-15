35e143ba8b8f3a883e4df86c18d16e20a2425acd
vivekkm1989 <52948784+vivekkm1989@users.noreply.github.com>
2022-11-15 13:44:44 +0530

    Update Line1.js

--- Line1.js
+++ Line1.js
@@ -58,7 +58,6 @@ set myDataSource(dataBinding){
      const myChart = echarts.init(this._root,"wight");
 var title=this._myDataSource.metadata.mainStructureMembers.measures_0.label + " per "+ this._myDataSource.metadata.dimensions.dimensions_0.description;
 const option = {
-animationDuration: 500,
   title: {
     text: title
   },
