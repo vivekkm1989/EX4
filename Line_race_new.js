ee7abd4e6e38d54c7322cc093e3573739aa64340
vivekkm1989 <52948784+vivekkm1989@users.noreply.github.com>
2022-11-23 12:06:35 +0530

    Add files via upload

---
+++ Line_race_new.js
@@ -0,0 +1,147 @@
+var getScriptPromisify=(src)=>{
+    return new Promise((resolve)=>{
+        $.getScript(src,resolve);
+    });
+};
+
+var ROOT_PATH = 'https://echarts.apache.org/examples';
+
+var chartDom = document.getElementById('main');
+var myChart = echarts.init(chartDom);
+var option;
+
+$.get(
+  ROOT_PATH + '/data/asset/data/life-expectancy-table.json',
+  function (_rawData) {
+    run(_rawData);
+  }
+);
+
+(function () {
+    const tmpl = document.createElement("template");
+    tmpl.innerHTML = `
+    <style>
+ </style>
+ <div id="root" style="width:100%; height:100%;">
+ </div>
+ `;
+
+    class LINERACE extends HTMLElement {
+
+constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({ mode: "open" });
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._root=this._shadowRoot.getElementById("root");
+	    this._props={};
+	 
+	    this.render();
+		
+}
+onCustomWidgetResize(width,height){
+	
+		this.render();	
+
+}
+
+set myDataSource(dataBinding){
+	this._myDataSource=dataBinding;
+	
+		this.render();
+	
+}
+async render() {
+await getScriptPromisify("https://cdn.bootcdn.net/ajax/libs/echarts/5.4.0/echarts.min.js");
+function run(_rawData) {
+  // var countries = ['Australia', 'Canada', 'China', 'Cuba', 'Finland', 'France', 'Germany', 'Iceland', 'India', 'Japan', 'North Korea', 'South Korea', 'New Zealand', 'Norway', 'Poland', 'Russia', 'Turkey', 'United Kingdom', 'United States'];
+  const countries = [
+    'Finland',
+    'France',
+    'Germany',
+    'Iceland',
+    'Norway',
+    'Poland',
+    'Russia',
+    'United Kingdom'
+  ];
+  const datasetWithFilters = [];
+  const seriesList = [];
+  echarts.util.each(countries, function (country) {
+    var datasetId = 'dataset_' + country;
+    datasetWithFilters.push({
+      id: datasetId,
+      fromDatasetId: 'dataset_raw',
+      transform: {
+        type: 'filter',
+        config: {
+          and: [
+            { dimension: 'Year', gte: 1950 },
+            { dimension: 'Country', '=': country }
+          ]
+        }
+      }
+    });
+    seriesList.push({
+      type: 'line',
+      datasetId: datasetId,
+      showSymbol: false,
+      name: country,
+      endLabel: {
+        show: true,
+        formatter: function (params) {
+          return params.value[3] + ': ' + params.value[0];
+        }
+      },
+      labelLayout: {
+        moveOverlap: 'shiftY'
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      encode: {
+        x: 'Year',
+        y: 'Income',
+        label: ['Country', 'Income'],
+        itemName: 'Year',
+        tooltip: ['Income']
+      }
+    });
+  });
+  option = {
+    animationDuration: 10000,
+    dataset: [
+      {
+        id: 'dataset_raw',
+        source: _rawData
+      },
+      ...datasetWithFilters
+    ],
+    title: {
+      text: 'Income of Germany and France since 1950'
+    },
+    tooltip: {
+      order: 'valueDesc',
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'category',
+      nameLocation: 'middle'
+    },
+    yAxis: {
+      name: 'Income'
+    },
+    grid: {
+      right: 140
+    },
+    series: seriesList
+  };
+  myChart.setOption(option);
+}
+
+option && myChart.setOption(option);
+};
+}
+
+        customElements.define('com-cg-line-race-new', LINERACE);  
+
+})();
