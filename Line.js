e5b488ee34bed31b093287f23e87a35eae682d7e
vivekkm1989 <52948784+vivekkm1989@users.noreply.github.com>
2023-06-23 11:44:08 +0530

    Update Line.js

--- Line.js
+++ Line.js
@@ -95,7 +95,7 @@ color:colors,
     boundaryGap: false
   },
 yAxis: {
-    type: '',
+    type: 'value',
 	name:'',
         nameLocation: 'middle',
         nameGap: 50
@@ -118,9 +118,6 @@ yAxis: {
       name: this._myDataSource.metadata.mainStructureMembers.measures_2.label,
       type: 'line',
 	smooth: 'true',
-		lineStyle: {
-             type: 'dashed'
-    }
       data: data.map(item => item.m3)
     },
 	  {
